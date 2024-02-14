2477de53f3596141f685795693d12a2ad5e9d7c2
Leonardo <102647115+DiskHD@users.noreply.github.com>
2024-02-14 00:47:03 -0600

    Update Mouse.java

--- Mouse.java
+++ Mouse.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package examenmetodos;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.util.Scanner;
 
-/**
- *
- * @author hdzdi
- */
 public class Mouse {
+    private Robot robot;
+    private int distanciaY;
+    private int distanciaX;
+    public Mouse(){
+        distanciaY=20;
+        distanciaX=200;
+    } 
+    public void moverMouse(int x,int y){
+        robot.mouseMove(x, y);
+    }
+    public void darClick(){
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+    public void seleccionarGrafica(int n){
+        int nuevaDistanciaY=distanciaY*n;
+        moverMouse(distanciaX,distanciaY);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        moverMouse(distanciaX,distanciaY);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        
+    }
+    
+    public void setDistanciaY(int d){
+        distanciaY=d;
+    }
+    public int getDistanciaY(){
+        return distanciaY;
+    }
+    public void setDistanciaX(int d){
+        distanciaX=d;
+    }
+    public int getDistanciaX(){
+        return distanciaX;
+    }
     
 }
